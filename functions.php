04b23d26feba56697fdee771711c0c0e116f2b2f
Pierre Charles <pierre.chls@gmail.com>
2018-03-11 18:36:19 +0100

    :sparkles: Adding theme options

--- functions.php
+++ functions.php
@@ -1,5 +1,34 @@
 <?php
 
+if( function_exists('acf_add_options_page') ) {
+
+  acf_add_options_page(array(
+		'page_title' 	=> 'Wine Space Settings',
+		'menu_title'	=> 'WS Settings',
+		'menu_slug' 	=> 'ws-general-settings',
+		'capability'	=> 'edit_posts',
+		'redirect'		=> false
+	));
+
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Wine Space Header Settings',
+		'menu_title'	=> 'Header',
+		'parent_slug'	=> 'ws-general-settings',
+	));
+
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Wine Space Footer Settings',
+		'menu_title'	=> 'Footer',
+		'parent_slug'	=> 'ws-general-settings',
+	));
+
+  acf_add_options_sub_page(array(
+		'page_title' 	=> 'Wine Space Flash Message Settings',
+		'menu_title'	=> 'Flash Message',
+		'parent_slug'	=> 'ws-general-settings',
+	));
+}
+
 // remove wp version param from any enqueued scripts
 function vc_remove_wp_ver_css_js( $src ) {
     if ( strpos( $src, 'ver=' ) )
