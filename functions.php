e64ff966e23889abc594c48e36136646e00dd4c8
Pierre Charles <pierre.chls@gmail.com>
2018-10-29 21:55:12 +0100

    :sparkles: Updating out of stock text

--- functions.php
+++ functions.php
@@ -1,8 +1,29 @@
 <?php
 
+// CUSTOMIZE ON SALE TEXT
+// add_filter('woocommerce_sale_flash', 'ev_display_on_sale_loop_woocommerce', 10, 3);
+// function ev_display_on_sale_loop_woocommerce($text, $post, $_product)
+// {
+//     return '<span class="onsale">PUT YOUR TEXT</span>';
+// }
+
+// CUSTOMIZE SOLD OUT
+// add_action( 'woocommerce_before_shop_loop_item_title', 'ev_display_sold_out_loop_woocommerce' );
+// function ev_display_sold_out_loop_woocommerce() {
+//     global $product;
+//
+//     if ( !$product->is_in_stock() ) {
+// 			// <div class="out_of_stock_badge_loop">Rupture de stock</div>
+//         echo '<span class="soldout">Bientôt dispo</span>';
+//     }
+// }
+
 function replaceDefaultWocommerceString( $translatedText, $text, $domain ) {
 
 	switch ( $translatedText ) {
+		case 'Rupture de stock' :
+			$translatedText = 'Bientôt disponible';
+			break;
 		case 'Produits apparentés' :
     case 'Related Products' :
 			$translatedText = 'Nos vins sélectionnés pour vous !';
