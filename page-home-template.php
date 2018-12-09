c2ee2aeb4fc27f5d887ac132d424f3ed4f24a1db
Pierre Charles <pierre.chls@gmail.com>
2018-12-09 10:07:46 +0100

    :bug: Fixing promo label homepage

--- page-home-template.php
+++ page-home-template.php
@@ -86,7 +86,7 @@
 
 	</style>
 
-	<div id="primary" class="primary-home content-area woocoomerce">
+	<div id="primary" class="primary-home content-area woocommerce">
 
         <div id="content" class="site-content" role="main">
 
